41a76a686fe67a03e5f3dc27987fd56b78fbea56
Tan Yin Loo <tanyinloo@mrc-asia.com>
2020-02-10 19:33:07 +0800

    make into a component project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Autocomplete from './widgets/Autocomplete.svelte';
+import Button from './widgets/Button.svelte';
+import Dialog from './widgets/Dialog.svelte';
+import Input from './widgets/Input.svelte';
+import NavigationDrawer from './widgets/NavigationDrawer.svelte';
+import Progress from './widgets/Progress.svelte';
+import Slider from './widgets/Slider.svelte';
+import Spinner from './widgets/Spinner.svelte';
+
+export {
+  Autocomplete, Button, Dialog,
+  Input,
+  NavigationDrawer,
+  Progress,
+  Slider,
+  Spinner,
+};
