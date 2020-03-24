ac75c282ba25d30f1f9e270ffa0e1fcd55df4622
Tan Yin Loo <tanyinloo@mrc-asia.com>
2020-03-24 10:58:33 +0800

    export Checkbox

--- src/index.js
+++ src/index.js
@@ -6,3 +6,4 @@ export { default as NavigationDrawer } from './widgets/NavigationDrawer.svelte';
 export { default as Progress } from './widgets/Progress.svelte';
 export { default as Slider } from './widgets/Slider.svelte';
 export { default as Spinner } from './widgets/Spinner.svelte';
+export { default as Checkbox } from './widgets/Checkbox.svelte';
