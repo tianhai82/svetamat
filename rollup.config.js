7e971c96c1abdf9a64d1c72428f8ccec19c81e27
Tan Yin Loo <tanyinloo@mrc-asia.com>
2020-05-05 12:32:31 +0800

    remove unused import

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
+// import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import pkg from './package.json';
