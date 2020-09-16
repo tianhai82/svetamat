e1cb7aee163d2aceb4696633ff85b7ed73bef683
Tan Yin Loo <tanyinloo@mrc-asia.com>
2020-09-16 14:52:56 +0800

    update install guide

--- src/components/Installation.svelte
+++ src/components/Installation.svelte
@@ -6,5 +6,5 @@
 <h2 class="text-xl font-semibold mt-6">Installation</h2>
 <p class="my-2">To create a new project, use the svetamat-template:</p>
 <p class="bg-gray-200 rounded p-5 font-light text-lg">
-  npx degit tianhai82/svetamat-template<br />svelte-app <br />cd svelte-app<br />yarn
+  npx degit tianhai82/svetamat-template svelte-app <br />cd svelte-app<br />yarn
 </p>
